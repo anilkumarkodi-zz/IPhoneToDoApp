9719f4d9f4d709f65cbeb195895bf5051bc5b860
Gaurav <gauravmi@thoughtworks.com>
2013-08-22 14:51:46 +0530

    [gaurav/anil] project structure created.

---
+++ ToDo/ToDo/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  ToDo
+//
+//  Created by gauravm on 22/08/13.
+//  Copyright (c) 2013 thoughtworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ ToDo/ToDoTests/ToDoTests.h
@@ -0,0 +1,13 @@
+//
+//  ToDoTests.h
+//  ToDoTests
+//
+//  Created by gauravm on 22/08/13.
+//  Copyright (c) 2013 thoughtworks. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ToDoTests : SenTestCase
+
+@end
